ea9a041b3cc266201ba338e0b1cd0e041ec141a9
kyriakosgi <kyriakosgi@gmail.com>
2017-05-30 00:22:50 +0300

    Implemented the process method

--- RLGameEcoClient/src/gr/eap/RLGameEcoClient/comm/MessageResponse.java
+++ RLGameEcoClient/src/gr/eap/RLGameEcoClient/comm/MessageResponse.java
@@ -1,5 +1,10 @@
 package gr.eap.RLGameEcoClient.comm;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import gr.eap.RLGameEcoClient.player.Player;
+
 public class MessageResponse extends Response {
 	private Message message;
 
@@ -17,7 +22,52 @@ public class MessageResponse extends Response {
 
 	@Override
 	public void process() {
-		// TODO Auto-generated method stub
+		Player sender = getMessage().getSender();
+		String currentMessage = getMessage().getText();
+		String regex = "(?i)((?!not).)*create.*game.*board.*size.*?(\\d+).*base.*size.*?(\\d+).*?(\\d+).*pawns.*";
+		Matcher matchCreateGame = Pattern.compile(regex,Pattern.CASE_INSENSITIVE).matcher(currentMessage);
+		String reply = "";
+		byte boardSize = 0, baseSize = 0, numberOfPawns = 0;
+		if (sender != null){
+			
+			if (matchCreateGame.matches()){
+				try {
+					boardSize = Byte.parseByte(matchCreateGame.group(2));
+					baseSize = Byte.parseByte(matchCreateGame.group(3));
+					numberOfPawns = Byte.parseByte(matchCreateGame.group(4));
+					reply = "Creating the game. Hope you join me!";
+				} catch (NumberFormatException e) {
+					reply = "I couldn't quite get the numbers, sorry :(";
+				}
+				
+			}
+			else if (matchCreateGame != null && matchCreateGame.group(1) != null){ // caught the word "not"
+				reply = "Not creating a game for now";
+			}
+			else{
+				reply = "OK";
+			}
+
+			if (reply != ""){
+				MessageCommand replyCommand = new MessageCommand();
+				replyCommand.setMessageText(reply);
+				replyCommand.getRecipientsIds().add(sender.getId());
+				replyCommand.setSocket(getSocket());
+				replyCommand.setUserId(getUserId());
+				replyCommand.send();
+			}
+		}
+		
+		if (boardSize != 0 && baseSize != 0 && numberOfPawns != 0){
+			//Create Game
+			CreateGameCommand createGameCommand = new CreateGameCommand();
+			createGameCommand.setBaseSize(baseSize);
+			createGameCommand.setBoardSize(boardSize);
+			createGameCommand.setNumberOfPawns(numberOfPawns);
+			createGameCommand.setSocket(getSocket());
+			createGameCommand.setUserId(getUserId());
+			createGameCommand.send();
+		}
 		
 	}
 
