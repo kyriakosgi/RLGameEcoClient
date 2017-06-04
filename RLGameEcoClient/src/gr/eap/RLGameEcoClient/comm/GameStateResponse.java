dc4b4e7bfd9cca0ddc9d38903a9d7e4a32fbe7f2
kyriakosgi <kyriakosgi@gmail.com>
2017-06-04 16:39:38 +0300

    Implemented the process method

--- RLGameEcoClient/src/gr/eap/RLGameEcoClient/comm/GameStateResponse.java
+++ RLGameEcoClient/src/gr/eap/RLGameEcoClient/comm/GameStateResponse.java
@@ -4,6 +4,9 @@ import java.util.UUID;
 
 import org.rlgame.gameplay.GameState;
 
+import gr.eap.RLGameEcoClient.Client;
+import gr.eap.RLGameEcoClient.game.Move;
+
 public class GameStateResponse extends Response {
 	private GameState state;
 	private UUID gameUid;
@@ -24,7 +27,31 @@ public class GameStateResponse extends Response {
 
 	@Override
 	public void process() {
-		// TODO Auto-generated method stub
+		//When game state is deserialized, pawns are created without their boardSize and baseSize properties set
+		//We will check if this is true and correct it
+		if (getState().getWhitePawns()[0].getBoardSize() == 0){
+			for (byte i = 0; i < Client.currentNumberOfPawns; i++){
+				getState().getWhitePawns()[i].setBoardSize(Client.currentBoardSize);
+				getState().getWhitePawns()[i].setBaseSize(Client.currentBaseSize);
+				getState().getBlackPawns()[i].setBoardSize(Client.currentBoardSize);
+				getState().getBlackPawns()[i].setBaseSize(Client.currentBaseSize);
+			}
+		}
+		if (getState().getTurn() == Client.machine.getId()){
+			
+			Move pickedMove = Client.machine.returnPickedMove(getState());
+			
+			MoveCommand mc = new MoveCommand();
+			mc.setSocket(getSocket());
+			mc.setPawnId(pickedMove.getPawn().getId());
+			mc.setToXCoord(pickedMove.getToSquare().getXCoord());
+			mc.setToYCoord(pickedMove.getToSquare().getYCoord());
+			mc.setUserId(getUserId());
+			mc.setGameUid(getGameUid());
+			mc.send();
+			
+
+		}
 		
 	}
 
