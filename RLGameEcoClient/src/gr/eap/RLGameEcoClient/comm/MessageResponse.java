f5c7607d7ab1cfbd335ae14bd11cf4de5fc23f95
kyriakosgi <kyriakosgi@gmail.com>
2017-06-13 01:19:45 +0300

    Added functionality for joining a game

--- RLGameEcoClient/src/gr/eap/RLGameEcoClient/comm/MessageResponse.java
+++ RLGameEcoClient/src/gr/eap/RLGameEcoClient/comm/MessageResponse.java
@@ -51,14 +51,18 @@ public class MessageResponse extends Response {
 				}
 				
 			}
-			else if (matchCreateGame != null && matchCreateGame.group(1) != null){ // caught the word "not"
-				reply = "Not creating a game for now";
-			}
+//			else if (matchCreateGame != null && matchCreateGame.toMatchResult().group(1).groupCount()>1 && matchCreateGame.group(1) != null){ // caught the word "not"
+//				reply = "Not creating a game for now";
+//			}
 			else{
-				String regexJoinGame = "join UID:(.+)";
+				String regexJoinGame = "join UID:(.+)\\sboardsize:(\\d+)\\sbasesize:(\\d+)\\spawns:(\\d+)";
 				Matcher matchJoinGame = Pattern.compile(regexJoinGame,Pattern.CASE_INSENSITIVE).matcher(currentMessage);
 				if (matchJoinGame.matches()){
 					joinGameUid = UUID.fromString(matchJoinGame.group(1));
+					boardSize = Byte.parseByte(matchJoinGame.group(2));
+					baseSize = Byte.parseByte(matchJoinGame.group(3));
+					numberOfPawns = Byte.parseByte(matchJoinGame.group(4));
+					
 					reply = "Joining game...";
 				}
 				else
@@ -67,37 +71,41 @@ public class MessageResponse extends Response {
 				}
 			}
 
-			if (reply != ""){
-				MessageCommand replyCommand = new MessageCommand();
-				replyCommand.setMessageText(reply);
-				replyCommand.getRecipientsIds().add(sender.getId());
-				replyCommand.setSocket(getSocket());
-				replyCommand.setUserId(getUserId());
-				replyCommand.send();
-			}
-			
-			if (joinGameUid != null){
-				JoinGameCommand joinCommand = new JoinGameCommand();
-				joinCommand.setGameUid(joinGameUid);
-				joinCommand.setRole(Role.BLACKPLAYER);
-				joinCommand.setId(0);
-				joinCommand.setSocket(getSocket());
-				joinCommand.setUserId(getUserId());
-				joinCommand.send();
-			}
+		}
+		
+		if (reply != ""){
+			MessageCommand replyCommand = new MessageCommand();
+			replyCommand.setMessageText(reply);
+			replyCommand.getRecipientsIds().add(sender.getId());
+			replyCommand.setSocket(getSocket());
+			replyCommand.setUserId(getUserId());
+			replyCommand.send();
 		}
 		
 		if (boardSize != 0 && baseSize != 0 && numberOfPawns != 0){
+			int ident, opponent, plies;
+			if (joinGameUid != null) {
+				ident = Settings.BLACK_PLAYER;
+				opponent = Settings.WHITE_PLAYER;
+				plies = Settings.PLAYER_B_PLIES;
+			}
+			else
+			{
+				ident = Settings.WHITE_PLAYER;
+				opponent = Settings.BLACK_PLAYER;
+				plies = Settings.PLAYER_W_PLIES;
+			}
+			
 			String playerType = Client.clientSettings.getProperty("playerType");
 			switch (playerType) {
 			case "RANDOM_PLAYER":
-				Client.machine = new RandomPlayer(Settings.WHITE_PLAYER);
+				Client.machine = new RandomPlayer(ident);
 				break;
 			case "MM_PLAYER":
-				Client.machine = new MMPlayer(Settings.WHITE_PLAYER, Settings.PLAYER_W_PLIES, Settings.BLACK_PLAYER, boardSize, baseSize, numberOfPawns);
+				Client.machine = new MMPlayer(ident, plies, opponent, boardSize, baseSize, numberOfPawns);
 				break;
 			case "RL_PLAYER":
-				Client.machine = new RLPlayer(Settings.WHITE_PLAYER, boardSize, baseSize, numberOfPawns);
+				Client.machine = new RLPlayer(ident, boardSize, baseSize, numberOfPawns);
 				break;
 
 			default:
@@ -107,15 +115,30 @@ public class MessageResponse extends Response {
 			Client.currentBaseSize = baseSize;
 			Client.currentBoardSize = boardSize;
 			Client.currentNumberOfPawns = numberOfPawns;
-			//Create Game
-			CreateGameCommand createGameCommand = new CreateGameCommand();
-			createGameCommand.setBaseSize(baseSize);
-			createGameCommand.setBoardSize(boardSize);
-			createGameCommand.setNumberOfPawns(numberOfPawns);
-			createGameCommand.setSocket(getSocket());
-			createGameCommand.setUserId(getUserId());
-			createGameCommand.send();
+			
+			if (joinGameUid != null) {
+				JoinGameCommand joinCommand = new JoinGameCommand();
+				joinCommand.setGameUid(joinGameUid);
+				joinCommand.setRole(Role.BLACKPLAYER);
+				joinCommand.setId(0);
+				joinCommand.setSocket(getSocket());
+				joinCommand.setUserId(getUserId());
+				joinCommand.send();
+			}
+			else
+			{
+				CreateGameCommand createGameCommand = new CreateGameCommand();
+				createGameCommand.setBaseSize(baseSize);
+				createGameCommand.setBoardSize(boardSize);
+				createGameCommand.setNumberOfPawns(numberOfPawns);
+				createGameCommand.setSocket(getSocket());
+				createGameCommand.setUserId(getUserId());
+				createGameCommand.send();
+			}
+			
+			
 		}
+		
 		
 	}
 
